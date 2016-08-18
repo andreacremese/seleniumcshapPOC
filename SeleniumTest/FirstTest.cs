b1cdcaf6b24dcec9157c1877a6fd4aad12b3125a
Andrea Cremese (Protiviti Inc) <v-ancrem@microsoft.com>
2016-08-18 15:39:09 -0700

    first commit

---
+++ SeleniumTest/FirstTest.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTest {
+
+    public class FirstTest {
+        private const string baseUrl = "https://run.plnkr.co/plunks/JmAWOqlJQ3JecBY8IJpD/";
+
+        [Fact]
+        public void goesToPage1() {
+            using (IWebDriver driver = new InternetExplorerDriver()) {
+                // arrange
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl(baseUrl);
+
+                IWebElement button = driver.FindElement(By.Id("1"));
+                // act
+                button.Click();
+                // assert
+                Assert.NotEqual(driver.Url.IndexOf("page1.html"), -1);
+                Assert.NotEmpty(driver.FindElements(By.XPath("//*[contains(text(), 'page 1')]")));
+                Assert.Empty(driver.FindElements(By.XPath("//*[contains(text(), 'page 2')]")));
+            }
+        }
+
+        [Fact]
+        public void goesToPage2() {
+            using (IWebDriver driver = new InternetExplorerDriver()) {
+                // arrange
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl(baseUrl);
+
+                IWebElement button = driver.FindElement(By.Id("2"));
+                // act
+                button.Click();
+                // assert
+                Assert.NotEqual(driver.Url.IndexOf("page2.html"), -1);
+                Assert.Empty(driver.FindElements(By.XPath("//*[contains(text(), 'page 1')]")));
+                Assert.NotEmpty(driver.FindElements(By.XPath("//*[contains(text(), 'page 2')]")));
+            }
+        }
+
+        [Fact]
+        public void requiresAcceptingConditions(){
+            using (IWebDriver driver = new InternetExplorerDriver()) {
+                // arrange
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl(baseUrl);
+                var inputs = driver.FindElements(By.TagName("input"));
+                Assert.False(inputs.Count < 4);
+                var firstName = inputs[0];
+                var checkbox = inputs[2];
+                var submit = inputs[3];
+                // Act
+                firstName.SendKeys("andrea");
+                checkbox.Click();
+                submit.Click();
+                // Assert
+                // wait up to 5 seconds in order to make sure the page loads
+                WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0,0, 5));
+                wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[contains(text(), 'well done andrea')]")));
+            }
+        }
+    }
+}
